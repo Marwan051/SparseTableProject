72d1792b85b1014f9c81e9be7fdb3b59d7934462
Marwan051 <115355087+Marwan051@users.noreply.github.com>
2023-11-06 15:42:05 +0200

    Add files via upload

---
+++ src/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Table table = new Table();
+		table.addCell(1,1);
+		table.addCell(2, 1);
+		table.showCourseStudents(1);;
+
+	}
+
+}
